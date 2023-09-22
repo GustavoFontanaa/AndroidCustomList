6361b80b5dba7343abd27a0ac40357db501ea10d
Gustavo Fontana <gustavoasf77@gmail.com>
2023-09-21 22:46:51 -0300

    Custom List

---
+++ app/src/main/java/com/example/customlist/adapter/ProdutoModel.java
@@ -0,0 +1,26 @@
+package com.example.customlist.adapter;
+
+public class ProdutoModel {
+
+    private String codigoProduto;
+    private String nomeProduto;
+    private String barrasProduto;
+
+    public ProdutoModel(final String codigoProduto, final String nomeProduto, final String barrasProduto) {
+        this.codigoProduto = codigoProduto;
+        this.nomeProduto = nomeProduto;
+        this.barrasProduto = barrasProduto;
+    }
+
+    public String getCodigoProduto() {
+        return codigoProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public String getBarrasProduto() {
+        return barrasProduto;
+    }
+}
